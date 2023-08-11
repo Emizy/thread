8af7767ca2a2125c28cc9e4f15f4487ac7419bc1
emizy <adenitiree@gmail.com>
2023-08-11 22:28:25 +0100

    - added user, post and comment model - updated setting with redis cache configuration

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = "core.User"
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -223,3 +223,14 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
 }
 
+# REDIS CONFIGURATION
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": config('REDIS_CACHE_LOCATION'),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "blog"
+    }
+}
--- core/models.py
+++ core/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    address = models.TextField(default="", null=True, blank=True)
+    avatar = models.ImageField(upload_to="profile", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.get_full_name()} - {self.email}'
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="%(class)s")
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    image = models.ImageField(upload_to='blog')
+    description = models.TextField(default='')
+    publish = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id} {self.title}"
+
+    class Meta:
+        verbose_name_plural = 'Posts'
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments", null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="%(class)s", null=True, blank=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name="%(class)s", null=True, blank=True)
+    body = models.TextField(default='')
+    timestamp = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        parent_id = self.parent.id if self.parent is not None else ''
+        name = self.post.title if self.post is not None else f'<Comment Reply> #{parent_id}'
+        return f"{name}"
+
+    class Meta:
+        verbose_name_plural = 'Comments'
